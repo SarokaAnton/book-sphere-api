966f348bb5c80d50b63a87533302e0e47e2d58b2
Anton Saroka <100620261+SarokaAnton@users.noreply.github.com>
2024-01-19 00:10:40 +0100

    feat (dev_spring_boot) provided application configuration

---
+++ src/main/java/com/projects/booksphere/utils/config/ApplicationConfig.java
@@ -0,0 +1,19 @@
+package com.projects.booksphere.utils.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.zalando.problem.jackson.ProblemModule;
+import org.zalando.problem.violations.ConstraintViolationProblemModule;
+
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper().registerModules(
+                new ProblemModule(),
+                new ConstraintViolationProblemModule(),
+                new JavaTimeModule());
+    }
+}
