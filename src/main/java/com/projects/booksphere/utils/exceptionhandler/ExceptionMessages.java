793fc7ec734980141bb47fcfad1522f734cdf58a
Anton Saroka <100620261+SarokaAnton@users.noreply.github.com>
2024-01-19 19:53:52 +0100

    feat (dev_spring_boot) provided class for exception messages

---
+++ src/main/java/com/projects/booksphere/utils/exceptionhandler/ExceptionMessages.java
@@ -0,0 +1,9 @@
+package com.projects.booksphere.utils.exceptionhandler;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ExceptionMessages {
+    public static final String GENRE_NOT_FOUND = "Genre with id %d not found";
+    public static final String GENRE_ALREADY_EXIST = "Genre with name %s already exist";
+}
