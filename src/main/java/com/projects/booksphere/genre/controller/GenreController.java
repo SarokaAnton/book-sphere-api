0f597915f072445e60f343e951100071a379a42f
Anton Saroka <100620261+SarokaAnton@users.noreply.github.com>
2024-01-20 17:48:25 +0100

    feat (genre) implemented genre controller

---
+++ src/main/java/com/projects/booksphere/genre/controller/GenreController.java
@@ -0,0 +1,44 @@
+package com.projects.booksphere.genre.controller;
+
+import com.github.fge.jsonpatch.mergepatch.JsonMergePatch;
+import com.projects.booksphere.genre.model.GenreDTO;
+import com.projects.booksphere.genre.service.GenreService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/genre")
+public class GenreController {
+    private final GenreService genreService;
+
+    @PostMapping
+    public ResponseEntity<GenreDTO> createGenre(@RequestBody @Valid GenreDTO genreDTO) {
+        return ResponseEntity.ok(genreService.createGenre(genreDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GenreDTO>> getAllGenres() {
+        return ResponseEntity.ok(genreService.getAllGenres());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GenreDTO> getGenreById(@PathVariable Long id) {
+        return ResponseEntity.ok(genreService.getGenreById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<GenreDTO> updateGenre(@PathVariable Long id, @RequestBody JsonMergePatch patch) {
+        return ResponseEntity.ok(genreService.updateGenre(id, patch));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteGenreById(@PathVariable Long id) {
+        genreService.deleteGenreById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
