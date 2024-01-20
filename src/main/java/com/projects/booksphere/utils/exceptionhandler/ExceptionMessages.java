d3eb1f2ded76d72f6abc086f4e48c9feef1aab82
Anton Saroka <100620261+SarokaAnton@users.noreply.github.com>
2024-01-20 17:45:57 +0100

    feat (genre) provided genre update exception message

--- src/main/java/com/projects/booksphere/utils/exceptionhandler/ExceptionMessages.java
+++ src/main/java/com/projects/booksphere/utils/exceptionhandler/ExceptionMessages.java
@@ -6,4 +6,5 @@ import lombok.experimental.UtilityClass;
 public class ExceptionMessages {
     public static final String GENRE_NOT_FOUND = "Genre with id %d not found";
     public static final String GENRE_ALREADY_EXIST = "Genre with name %s already exist";
+    public static final String GENRE_UPDATE_EXCEPTION_MESSAGE = "Bad genre update request";
 }
