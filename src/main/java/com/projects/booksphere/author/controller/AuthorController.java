c948b4bfb95568f999c34ca80538e012fbbfb991
Anton Saroka <100620261+SarokaAnton@users.noreply.github.com>
2024-01-24 18:00:27 +0100

    feat (author) provided controller for author

---
+++ src/main/java/com/projects/booksphere/author/controller/AuthorController.java
@@ -0,0 +1,44 @@
+package com.projects.booksphere.author.controller;
+
+import com.github.fge.jsonpatch.mergepatch.JsonMergePatch;
+import com.projects.booksphere.author.model.AuthorDTO;
+import com.projects.booksphere.author.service.AuthorService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/author")
+public class AuthorController {
+    private final AuthorService authorService;
+
+    @PostMapping
+    public ResponseEntity<AuthorDTO> createAuthor(@RequestBody @Valid AuthorDTO authorDTO) {
+        return ResponseEntity.ok(authorService.createAuthor(authorDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AuthorDTO>> getAllAuthor() {
+        return ResponseEntity.ok(authorService.getAllAuthors());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AuthorDTO> getAuthorById(@PathVariable Long id) {
+        return ResponseEntity.ok(authorService.getAuthorById(id));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<AuthorDTO> updateAuthor(@PathVariable Long id, @RequestBody JsonMergePatch patch) {
+        return ResponseEntity.ok(authorService.updateAuthor(id, patch));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAuthorById(@PathVariable Long id) {
+        authorService.deleteAuthorById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
