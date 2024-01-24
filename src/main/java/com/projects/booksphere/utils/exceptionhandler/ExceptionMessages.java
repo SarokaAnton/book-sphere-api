be5ea2b76634e6b34f0956e05676f277b3114b89
Anton Saroka <100620261+SarokaAnton@users.noreply.github.com>
2024-01-24 17:51:49 +0100

    feat (dev_spring_boot) provided exception messages for books, tags and authors

--- src/main/java/com/projects/booksphere/utils/exceptionhandler/ExceptionMessages.java
+++ src/main/java/com/projects/booksphere/utils/exceptionhandler/ExceptionMessages.java
@@ -5,6 +5,15 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class ExceptionMessages {
     public static final String GENRE_NOT_FOUND = "Genre with id %d not found";
-    public static final String GENRE_ALREADY_EXIST = "Genre with name %s already exist";
+    public static final String GENRE_ALREADY_EXIST = "Genre with name %s already exists";
     public static final String GENRE_UPDATE_EXCEPTION_MESSAGE = "Bad genre update request";
+    public static final String BOOK_NOT_FOUND = "Book with id %d not found";
+    public static final String BOOK_ALREADY_EXIST = "Such book already exists";
+    public static final String BOOK_UPDATE_EXCEPTION_MESSAGE = "Bad book update request";
+    public static final String AUTHOR_NOT_FOUND = "Author with id %d not found";
+    public static final String AUTHOR_ALREADY_EXIST = "This author already exists";
+    public static final String AUTHOR_UPDATE_EXCEPTION_MESSAGE = "Bad author update request";
+    public static final String TAG_NOT_FOUND = "Tag with id %d not found";
+    public static final String TAG_ALREADY_EXIST = "Tag with name %s already exists";
+    public static final String TAG_UPDATE_EXCEPTION_MESSAGE = "Bad tag update request";
 }
