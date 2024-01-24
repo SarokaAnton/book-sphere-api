d91182976be3e8a2ba919cdf40b41d074f5a2f2a
Anton Saroka <100620261+SarokaAnton@users.noreply.github.com>
2024-01-24 17:52:50 +0100

    feat (dev_spring_boot) provided mapper for DTO and entity classes for books, tags and authors

--- src/main/java/com/projects/booksphere/utils/mappers/EntityToDtoMapper.java
+++ src/main/java/com/projects/booksphere/utils/mappers/EntityToDtoMapper.java
@@ -1,7 +1,13 @@
 package com.projects.booksphere.utils.mappers;
 
+import com.projects.booksphere.author.model.Author;
+import com.projects.booksphere.author.model.AuthorDTO;
+import com.projects.booksphere.book.model.Book;
+import com.projects.booksphere.book.model.BookDTO;
 import com.projects.booksphere.genre.model.Genre;
 import com.projects.booksphere.genre.model.GenreDTO;
+import com.projects.booksphere.tag.model.Tag;
+import com.projects.booksphere.tag.model.TagDTO;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
@@ -9,4 +15,16 @@ public interface EntityToDtoMapper {
     GenreDTO toGenreDTO(Genre genre);
 
     Genre toGenre(GenreDTO genreDTO);
+
+    BookDTO toBookDTO(Book book);
+
+    Book toBook(BookDTO bookDTO);
+
+    AuthorDTO toAuthorDTO(Author author);
+
+    Author toAuthor(AuthorDTO authorDTO);
+
+    TagDTO toTagDTO(Tag tag);
+
+    Tag toTag(TagDTO tagDTO);
 }
